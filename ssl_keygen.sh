7f36ca28c07e437f40dc8a386463aae29ee2d315
pheineke <heineke.pe@gmail.com>
2025-03-15 23:58:57 +0100

    updated keygen

--- ssl_keygen.sh
+++ ssl_keygen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -nodes
+openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -nodes -subj "/C=/ST=/L=/O=/OU=/CN="
